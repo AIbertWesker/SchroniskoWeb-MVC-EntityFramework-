0e4c48d630982d6afcb20671ae801a83de7a9a7d
AIbertWesker <mbubel999@gmail.com>
2024-01-11 11:22:45 +0100

    edycja/usuwanie worki

--- SchroniskoWebowka/Controllers/PsyController.cs
+++ SchroniskoWebowka/Controllers/PsyController.cs
@@ -56,9 +56,75 @@ namespace SchroniskoWebowka.Controllers
                 };
                 await _context.Pies.AddAsync(pies);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(decimal id)
+        {
+			var pies = await _context.Pies.FirstOrDefaultAsync(x => x.PiesId == id);
+            if (pies != null)
+            {
+                var viewModel = new UpdatePiesViewModel()
+                {
+                    PiesId = pies.PiesId,
+                    PiesNazwa = pies.PiesNazwa,
+                    PiesCzySuka = pies.PiesCzySuka,
+                    PiesDataUrodzenia = pies.PiesDataUrodzenia,
+                    PiesZdjecie = pies.PiesZdjecie,
+                    PiesCzyDostepny = pies.PiesCzyDostepny,
+                    PiesKolorWzorSiersci = pies.PiesKolorWzorSiersci,
+                    PiesOpis = pies.PiesOpis,
+                    StrefaId = pies.StrefaId,
+                    CharakterId = pies.CharakterId,
+                    RasaId = pies.RasaId,
+                    WiekId = pies.WiekId
+                };
+                ViewBag.Strefa = new SelectList(_context.Strefas, "StrefaId", "StrefaNazwa");
+                ViewBag.Rasa = new SelectList(_context.Rasas, "RasaId", "RasaNazwa");
+                ViewBag.Charakter = new SelectList(_context.Charakters, "CharakterId", "CharakterNazwa");
+                ViewBag.Wiek = new SelectList(_context.Wieks, "WiekId", "WiekNazwa");
 
+                return await Task.Run(() => View("Edit", viewModel));
+            }
+            return RedirectToAction("Index", "Home");  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdatePiesViewModel model)
+        {
+            var piesToUpdate = await _context.Pies.FirstOrDefaultAsync(x => x.PiesId == model.PiesId);
+
+            if (piesToUpdate != null)
+            {
+                piesToUpdate.PiesNazwa = model.PiesNazwa;
+                piesToUpdate.PiesCzySuka = model.PiesCzySuka;
+                piesToUpdate.PiesDataUrodzenia = model.PiesDataUrodzenia;
+                piesToUpdate.PiesCzyDostepny = model.PiesCzyDostepny;
+                piesToUpdate.PiesKolorWzorSiersci = model.PiesKolorWzorSiersci;
+                piesToUpdate.PiesOpis = model.PiesOpis;
+                piesToUpdate.StrefaId = model.StrefaId;
+                piesToUpdate.CharakterId = model.CharakterId;
+                piesToUpdate.RasaId = model.RasaId;
+                piesToUpdate.WiekId = model.WiekId;
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdatePiesViewModel model)
+        {
+            var pies = await _context.Pies.FindAsync(model.PiesId);
+            if (pies != null)
+            {
+                _context.Pies.Remove(pies);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
---
+++ SchroniskoWebowka/Models/UpdatePiesViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SchroniskoWebowka.Models
+{
+	public class UpdatePiesViewModel
+	{
+        public decimal PiesId { get; set; }
+        public decimal StrefaId { get; set; }
+        public decimal? CharakterId { get; set; }
+        public decimal RasaId { get; set; }
+        public decimal WiekId { get; set; }
+        public string? PiesNazwa { get; set; }
+        public bool PiesCzySuka { get; set; }
+        public DateTime? PiesDataUrodzenia { get; set; }
+        public byte[]? PiesZdjecie { get; set; }
+        public bool PiesCzyDostepny { get; set; }
+        public string? PiesKolorWzorSiersci { get; set; }
+        public string? PiesOpis { get; set; }
+    }
+}
